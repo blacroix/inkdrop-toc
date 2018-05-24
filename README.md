c0301dc32e6c5c282623f612bacdd649dc90e08f
Benjamin Lacroix <b.lacroix@betclicgroup.com>
2018-05-24 10:52:53 +0200

    feat(*): toc plugin for inkdrop based on remark-toc

---
+++ README.md
@@ -0,0 +1,14 @@
+# Table of Contents for Inkdrop
+
+A plugin for creating table of contents using [remark-toc](https://github.com/remarkjs/remark-toc).
+
+# Usage
+
+Simply add `Table of Contents` to your Markdown.
+
+From [remark-toc](https://github.com/remarkjs/remark-toc):
+
+> * Looks for the first heading containing 'Table of Contents', 'toc', or 'table-of-contents' (case insensitive, supports alt/title attributes for links and images too)
+> * Removes all following contents until an equal or higher heading is found
+> * Inserts a list representation of the hierarchy of following headings
+> * Adds links to following headings, using the same slugs as GitHub
---
+++ src/inkdrop-toc.js
@@ -0,0 +1,94 @@
+import React from 'react'; // eslint-disable-line
+import Toc from 'remark-toc';
+import $ from 'jquery';
+
+class TocPlugin {
+
+  constructor(MDEPreview) {
+    this.MDEPreview = MDEPreview;
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  start() {
+    if (this.MDEPreview) {
+      this.MDEPreview.remarkPlugins.push(Toc);
+      this.setTocComponent();
+    }
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  stop() {
+    if (this.MDEPreview) {
+      this.MDEPreview.remarkPlugins = this.MDEPreview.remarkPlugins.filter((plugin) => plugin !== Toc);
+      this.unsetTocComponent();
+    }
+  }
+
+  setTocComponent() {
+    // noinspection JSUnresolvedVariable
+    const OrigA = this.MDEPreview.remarkReactOptions.remarkReactComponents.a;
+    const hash = '#';
+    // noinspection JSUnresolvedVariable
+    this.MDEPreview.remarkReactOptions.remarkReactComponents.a = (props) => {
+      if (props.href && props.href.startsWith(hash)) {
+        return <a {...props} onClick={() => this.onTocEntryClick(props)}>{props.children}</a>;
+      } else if (OrigA) {
+        return <OrigA {...props}>{props.children}</OrigA>;
+      } else {
+        return <a {...props}>{props.children}</a>;
+      }
+    };
+  }
+
+  unsetTocComponent() {
+    // noinspection JSUnresolvedVariable
+    const OrigA = this.MDEPreview.remarkReactOptions.remarkReactComponents.a;
+    const hash = '#';
+    // noinspection JSUnresolvedVariable
+    this.MDEPreview.remarkReactOptions.remarkReactComponents.a = (props) => {
+      if (props.href && props.href.startsWith(hash)) {
+        if (OrigA) {
+          return <OrigA {...props}>{props.children}</OrigA>;
+        } else {
+          return <a {...props}>{props.children}</a>;
+        }
+      }
+    };
+  }
+
+  // noinspection JSMethodCanBeStatic
+  onTocEntryClick(props) {
+    const duration = 200;
+    const entry = `#user-content-${props.href.substr(1, props.href.length)}`;
+    const offset = 300;
+    const mdePreview = $('.mde-preview');
+    // noinspection JSUnresolvedFunction
+    mdePreview.stop().animate({scrollTop: $(entry).first().offset().top + mdePreview.scrollTop() - offset}, duration);
+  }
+
+}
+
+// noinspection JSUnusedGlobalSymbols
+const tocPlugin = {
+
+  toc: null,
+
+  activate() {
+    // noinspection JSUnresolvedVariable
+    const {MDEPreview} = inkdrop.components.classes; // eslint-disable-line
+    if (MDEPreview) {
+      this.toc = new TocPlugin(MDEPreview);
+      this.toc.start();
+    }
+  },
+
+  deactivate() {
+    // noinspection JSUnresolvedVariable
+    this.toc.stop();
+    this.toc = null;
+  },
+
+};
+
+// noinspection JSUnusedGlobalSymbols
+export default tocPlugin;
